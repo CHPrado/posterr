032f8ae06e43093733f893209afd0ea695955a16
Christian Prado <christianprado.contato@gmail.com>
2022-04-30 20:32:15 -0300

    Add Home and User Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
-function Home() {
+import { UserProps } from "../../interfaces";
+import { fakeApi } from "../../services";
+
+const Home = () => {
+  const [loggedUser, setLoggedUser] = useState<UserProps>();
+
+  useEffect(() => {
+    fakeApi.getUser().then((user) => setLoggedUser(user));
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <Link to="/user/test">Link to profile</Link>
-      <Outlet />
+      <h2>
+        Welcome to the Home Page
+        <Link to={`/user/${loggedUser?.id}`}>{loggedUser?.name}</Link>
+      </h2>
+      <nav>
+        <Link to="/">Posts</Link>
+        <Link to="/following">Who you follow</Link>
+      </nav>
+
+      <Outlet context={loggedUser} />
     </div>
   );
-}
+};
 
 export default Home;
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function User() {
+import { UserProps } from "../../interfaces";
+import { fakeApi } from "../../services";
+
+const User = () => {
+  const { id } = useParams<"id">();
+  const [user, setUser] = useState<UserProps>();
+
+  console.log(id);
+  useEffect(() => {
+    fakeApi.getUserById(Number(id)).then((user) => setUser(user));
+  }, [id]);
+
   return (
     <div>
-      <h3>User Profile Page</h3>
+      <img src={user?.avatar} alt={`${user?.id}-user-avatar`} />
+      <span>{user?.name}</span>
+
+      <p>Followers: {user?.followersIds.length}</p>
+      <p>Following: {user?.followingIds.length}</p>
     </div>
   );
-}
+};
 
 export default User;
