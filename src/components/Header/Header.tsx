930d4b56a613c7564f988a9ab39f6c14aee97d5c
Christian Prado <christianprado.contato@gmail.com>
2022-04-30 23:26:25 -0300

    Adjust header to have fixed position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,23 +12,25 @@ interface HeaderParams {
 
 const Header: FC<HeaderParams> = ({ loggedUser }) => {
   return (
-    <div className="header-container">
-      <span className="poster-title">Posterr</span>
+    <div className="header-wrapper">
+      <div className="header-container">
+        <span className="poster-title">Posterr</span>
 
-      <div className="header-buttons-container">
-        <Link to={`/`}>
-          <FaHome size={24} />
-          <span>Home</span>
-        </Link>
-        <Link to={`/user/${loggedUser?.id}`}>
-          <FaUser size={24} />
-          <span>Profile</span>
-        </Link>
-      </div>
+        <div className="header-buttons-container">
+          <Link to={`/`}>
+            <FaHome size={24} />
+            <span>Home</span>
+          </Link>
+          <Link to={`/user/${loggedUser?.id}`}>
+            <FaUser size={24} />
+            <span>Profile</span>
+          </Link>
+        </div>
 
-      <div className="header-user-container">
-        <img src={loggedUser?.avatar} alt={"logged-user-avatar"} />
-        <span>{loggedUser?.name}</span>
+        <div className="header-user-container">
+          <img src={loggedUser?.avatar} alt={"logged-user-avatar"} />
+          <span>{loggedUser?.name}</span>
+        </div>
       </div>
     </div>
   );
